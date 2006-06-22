0f9539a4cd3632699b5b827e2a3c34ca79a22784
Lee Begg <lee@thousandparsec.net>
2006-06-22 10:54:27 +0000

    Added test program.
    Doesn't do anything yet, but it is used to make sure the programs will compile and link
    with it.
    .

---
+++ test/test.cpp
@@ -0,0 +1,19 @@
+//
+// C++ Implementation: test
+//
+// Description: 
+//
+//
+// Author: Lee Begg <llnz@paradise.net.nz>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include <tprl/rlcommand.h>
+
+int main(int argc, char** argv){
+
+
+    return 0;
+}
