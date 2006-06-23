02bf0212eee74055b91e67f4451b45f434c72cf1
Lee Begg <lee@thousandparsec.net>
2006-06-23 09:11:28 +0000

    Bit of content to the test program.
    Not finished yet, but gives an idea what it will look like.
    .

--- test/test.cpp
+++ test/test.cpp
@@ -11,9 +11,44 @@
 //
 
 #include <tprl/rlcommand.h>
+#include <tprl/commandalias.h>
+#include <tprl/commandcategory.h>
+#include <tprl/console.h>
+
+using namespace tprl;
+
+bool timetoend;
+
+class QuitCommand : public RLCommand{
+  public:
+    QuitCommand() : RLCommand(){
+      name = "quit";
+      help = "Quit and shutdown the server.";
+    }
+    void action(const std::string& cmdline){
+      timetoend = true;
+    }
+};
 
 int main(int argc, char** argv){
+  timetoend = false;
+  Console *console = Console::getConsole();
+  
+  std::set<RLCommand*> commands;
+  
+  RLCommand* quit = new QuitCommand();
+  commands.insert(quit);
+  CommandAlias* alias = new CommandAlias("exit");
+  alias->setTarget(quit);
+  commands.insert(alias);
+  
+  console->setCommandSet( &commands);
+  
+  while(!timetoend){
+    console->readLine();
+  }
 
-
-    return 0;
+  delete console;
+  
+  return 0;
 }
