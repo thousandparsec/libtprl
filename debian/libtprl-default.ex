d8aac8a958793d37f92da6e1d1a51f051935978d
Paul Schulz <paul@flik.(none)>
2007-05-20 18:51:44 +0930

    Initial debianised after dh-make

---
+++ debian/libtprl-default.ex
@@ -0,0 +1,10 @@
+# Defaults for libtprl initscript
+# sourced by /etc/init.d/libtprl
+# installed at /etc/default/libtprl by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
