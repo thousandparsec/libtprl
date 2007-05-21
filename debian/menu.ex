836bf86dae05d90affd2a68849778866e6b95ece
Lee Begg <llnz@paradise.net.nz>
2007-05-21 20:16:27 +1200

    Added Debian patch from Paul Schulz. Thanks Paul!

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libtprl):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="libtprl" command="/usr/bin/libtprl"
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibtprl 0.1.2a libtprl (>> 0.1.2a-0), libtprl (<< 0.1.2a-99)
